6dc92ccd1fe4c7dcb734931f59373126dd8dfc9c
manishabanka <manisha.banka.526@gmail.com>
2023-12-14 19:00:43 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import EditablePage from "./components/editablePage";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <h1 className="Logo">notion.clone</h1>
+    <p className="Intro">
+      Helloo{" "}
+      <span role="img" aria-label="greetings" className="Emoji">
+        👋
+      </span>{" "}
+      You can add content below. Type <span className="Code">/</span> to see
+      available elements.
+    </p>
+    <EditablePage />
+  </React.StrictMode>,
+  rootElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
